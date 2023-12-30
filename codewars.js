e3905d25af9deacd637ed195923de4aa3521f040
olepak <oleksandr.lepak@gmail.com>
2023-12-30 20:21:09 +0200

    7kyu Highest and lowest

--- codewars.js
+++ codewars.js
@@ -1,3 +1,21 @@
-function evenOrOdd(number) {}
+numbers = "8 3 -5 42 -1 0 0 -9 4 7  -4";
 
-evenOrOdd(2);
+const stringNumbersArray = numbers.split(" ");
+const numbersArray = stringNumbersArray.map((value) => +value);
+let lowest = numbersArray
+	.reduce((acc, value) => {
+		if (acc < value) {
+			return acc;
+		} else return value;
+	}, Infinity)
+	.toString();
+
+let highest = numbersArray.reduce((acc, value) => {
+	if (acc > value) {
+		return acc;
+	} else return value;
+}, -Infinity);
+
+// Best practice
+numbers = numbers.split(" ");
+`${Math.max(...numbers)} ${Math.min(...numbers)}`;
