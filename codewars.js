038387fb56acd7b87aab018809e6c2f98b8878f5
olepak <oleksandr.lepak@gmail.com>
2024-01-04 17:09:06 +0200

    8Kyu Total amount of points complete
    Usage of destructuring in the reduce.method

--- codewars.js
+++ codewars.js
@@ -1,21 +1,29 @@
-numbers = "8 3 -5 42 -1 0 0 -9 4 7  -4";
+function points(games) {
+	let sum = 0;
+	for (let i = 0; i < 10; i++) {
+		let singleScore = games.pop();
+		const result = singleScore.split("").map((value) => +value);
+		sum += result[0] > result[2] ? 3 : result[0] === result[2] ? 1 : 0; //?
+	}
+}
 
-const stringNumbersArray = numbers.split(" ");
-const numbersArray = stringNumbersArray.map((value) => +value);
-let lowest = numbersArray
-	.reduce((acc, value) => {
-		if (acc < value) {
-			return acc;
-		} else return value;
-	}, Infinity)
-	.toString();
+points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]);
+points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"]);
+points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]);
 
-let highest = numbersArray.reduce((acc, value) => {
-	if (acc > value) {
-		return acc;
-	} else return value;
-}, -Infinity);
+// The most concise solution using destructuring method
 
-// Best practice
-numbers = numbers.split(" ");
-`${Math.max(...numbers)} ${Math.min(...numbers)}`;
+const points2 = (g) => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0);
+const gameScores = [
+	"1:0",
+	"2:0",
+	"3:0",
+	"4:0",
+	"2:1",
+	"3:1",
+	"4:1",
+	"3:2",
+	"4:2",
+	"4:3",
+];
+console.log(points2(["2:1"]));
