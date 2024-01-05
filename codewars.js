f706590dbe702a8a7605d9e58c0db2077d545f65
olepak <oleksandr.lepak@gmail.com>
2024-01-05 17:20:17 +0200

    7Kyu Mumbling

--- codewars.js
+++ codewars.js
@@ -1,29 +1,17 @@
-function points(games) {
-	let sum = 0;
-	for (let i = 0; i < 10; i++) {
-		let singleScore = games.pop();
-		const result = singleScore.split("").map((value) => +value);
-		sum += result[0] > result[2] ? 3 : result[0] === result[2] ? 1 : 0; //?
+function accum(s) {
+	let result = "";
+	let firstLetter = s.charAt(0).toUpperCase();
+	let nextLetters = "";
+	for (let i = 1; i < s.length; i++) {
+		nextLetters +=
+			"-" + s.charAt(i).toUpperCase() + s.charAt(i).toLowerCase().repeat(i);
 	}
+	return result + firstLetter + nextLetters;
 }
+accum("zpglnRxqenU");
+accum("NyffsGeyylB");
 
-points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]);
-points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"]);
-points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"]);
-
-// The most concise solution using destructuring method
-
-const points2 = (g) => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0);
-const gameScores = [
-	"1:0",
-	"2:0",
-	"3:0",
-	"4:0",
-	"2:1",
-	"3:1",
-	"4:1",
-	"3:2",
-	"4:2",
-	"4:3",
-];
-console.log(points2(["2:1"]));
+// Best practice solution
+function accum1(n) {}
+accum1("zpglnRxqenU");
+accum1("NyffsGeyylB");
