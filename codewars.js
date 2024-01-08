07f30782f78425ab97a501ccc80d630ba54b8f40
olepak <oleksandr.lepak@gmail.com>
2024-01-08 09:48:48 +0200

    7 kyu Regex validate PINcode

--- codewars.js
+++ codewars.js
@@ -1,1 +1,7 @@
-
+function validatePIN(pin) {
+	if (pin.match(/[a-z]/g) !== null || pin.length !== 4 || pin.length !== 6) {
+		return false;
+	} else return true;
+}
+validatePIN("1234");
+validatePIN("12345");
