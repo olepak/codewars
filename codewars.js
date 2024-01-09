920cdbe1fb90e7d4f135505049ed6ce214cf784e
olepak <oleksandr.lepak@gmail.com>
2024-01-09 17:01:19 +0200

    7 kyu Regex validate PIN code complete

--- codewars.js
+++ codewars.js
@@ -1,7 +1,20 @@
 function validatePIN(pin) {
-	if (pin.match(/[a-z]/g) !== null || pin.length !== 4 || pin.length !== 6) {
+	if (pin.match(/\D/g) !== null || (pin.length !== 4 && pin.length !== 6)) {
 		return false;
 	} else return true;
 }
 validatePIN("1234");
 validatePIN("12345");
+validatePIN("-1.232");
+validatePIN(".232");
+
+// Best practice
+
+function validatePIN2(pin) {
+	return /^(\d{4}|\d{6})$/.test(pin);
+}
+
+validatePIN2("1234");
+validatePIN2("12345");
+validatePIN2("-1,232");
+validatePIN2(".232");
