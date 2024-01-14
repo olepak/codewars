3d84920def51e2bafee0c22fa3a489bea90f159f
olepak <oleksandr.lepak@gmail.com>
2024-01-14 17:41:08 +0200

    7kyu Isogram
    Check whether a string contain only unique characters.
    Usage of RexEx.

--- codewars.js
+++ codewars.js
@@ -1,24 +1,21 @@
-function longest(s1, s2) {
-	let sorted = (s1 + s2).split("").sort().join("");
+function isIsogram(str) {
+	let checkString = str.toLowerCase();
 	let unique = "";
-	for (char of sorted) {
+	for (char of checkString) {
 		if (unique.indexOf(char) === -1) {
 			unique += char;
 		}
 	}
-	return unique;
+	return checkString.length === unique.length;
 }
-
-longest("aretheyhere", "yestheyarehere");
-longest("loopingisfunbutdangerous", "lessdangerousthancoding");
+isIsogram("Dermatoglyphics");
+isIsogram("aba");
+isIsogram("isIsogram");
 
 // Best practice without usage of Set
-
-function longest2(s1, s2) {
-	let sorted = (s1 + s2).split("").sort();
-	return sorted
-		.filter((value, index, arr) => value !== arr[index - 1])
-		.join("");
+function isIsogram2(str) {
+	return !/(\w).*\1/g.test(str);
 }
-longest2("aretheyhere", "yestheyarehere");
-longest2("loopingisfunbutdangerous", "lessdangerousthancoding");
+isIsogram2("Dermatoglyphics");
+isIsogram2("aba");
+isIsogram2("isIsogram");
