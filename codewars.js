08dfc03e2404722b26720089c6ef4f777eb935e4
olepak <oleksandr.lepak@gmail.com>
2024-01-18 17:23:43 +0200

    7 kyu Sum of numbers
    Sum of numbers between specified ones. Use of Math.max and min

--- codewars.js
+++ codewars.js
@@ -1,21 +1,29 @@
-function isIsogram(str) {
-	let checkString = str.toLowerCase();
-	let unique = "";
-	for (char of checkString) {
-		if (unique.indexOf(char) === -1) {
-			unique += char;
+function getSum(a, b) {
+	let sum = 0;
+	if (a < b) {
+		for (let i = a; i <= b; i++) {
+			sum += i;
 		}
-	}
-	return checkString.length === unique.length;
+		return sum;
+	} else if (a > b) {
+		for (let j = b; j <= a; j++) {
+			sum += j;
+		}
+		return sum;
+	} else return a;
 }
-isIsogram("Dermatoglyphics");
-isIsogram("aba");
-isIsogram("isIsogram");
+getSum(0, -1);
+getSum(0, 1);
+getSum(2, 2);
+getSum(-321, 123);
 
-// Best practice without usage of Set
-function isIsogram2(str) {
-	return !/(\w).*\1/g.test(str);
+// Best practice
+function getSum2(a, b) {
+	let min = Math.min(a, b);
+	let max = Math.max(a, b);
+	return ((max - min + 1) * (min + max)) / 2;
 }
-isIsogram2("Dermatoglyphics");
-isIsogram2("aba");
-isIsogram2("isIsogram");
+getSum2(0, -1);
+getSum2(0, 1);
+getSum2(2, 2);
+getSum2(-321, 123);
