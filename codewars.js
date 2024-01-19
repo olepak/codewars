1c3e742f1d8ba4de948de4ca7dd8f5fa8b0ccafb
olepak <oleksandr.lepak@gmail.com>
2024-01-19 17:33:13 +0200

    8 kyu Alternating character case

--- codewars.js
+++ codewars.js
@@ -1,29 +1,34 @@
-function getSum(a, b) {
-	let sum = 0;
-	if (a < b) {
-		for (let i = a; i <= b; i++) {
-			sum += i;
-		}
-		return sum;
-	} else if (a > b) {
-		for (let j = b; j <= a; j++) {
-			sum += j;
-		}
-		return sum;
-	} else return a;
-}
-getSum(0, -1);
-getSum(0, 1);
-getSum(2, 2);
-getSum(-321, 123);
+String.prototype.toAlternatingCase = function () {
+	let toggledString = "";
+	for (let i = 0; i < this.length; i++) {
+		if (this.charAt(i) === this.charAt(i).toUpperCase()) {
+			toggledString += this.charAt(i).toLowerCase();
+		} else toggledString += this.charAt(i).toUpperCase();
+	}
+	return toggledString;
+};
+let string = "hello world";
+let alternatedString = string.toAlternatingCase();
+let string2 = "HELLO WORLD";
+let alternatedString2 = string2.toAlternatingCase();
+let string3 = "HeLLo WoRlD";
+let alternatedString3 = string3.toAlternatingCase();
+let string4 = "12345";
+let alternatedString4 = string4.toAlternatingCase();
 
 // Best practice
-function getSum2(a, b) {
-	let min = Math.min(a, b);
-	let max = Math.max(a, b);
-	return ((max - min + 1) * (min + max)) / 2;
-}
-getSum2(0, -1);
-getSum2(0, 1);
-getSum2(2, 2);
-getSum2(-321, 123);
+String.prototype.toAlternatingCase1 = function () {
+	return this.split("")
+		.map((a) =>
+			a === a.toUpperCase() ? a.toLocaleLowerCase() : a.toUpperCase()
+		)
+		.join("");
+};
+let string1 = "hello world";
+let alternatedString1 = string.toAlternatingCase1();
+let string21 = "HELLO WORLD";
+let alternatedString21 = string2.toAlternatingCase1();
+let string31 = "HeLLo WoRlD";
+let alternatedString31 = string3.toAlternatingCase1();
+let string41 = "12345";
+let alternatedString41 = string4.toAlternatingCase1();
