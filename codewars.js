94a8cb70695f8f875eccdc77c678e383ed96497e
olepak <oleksandr.lepak@gmail.com>
2024-02-06 07:07:26 +0200

    upd:

--- codewars.js
+++ codewars.js
@@ -1,6 +1,16 @@
 function removeSmallest(numbers) {
+	let resultArray = numbers.concat();
 	let min = Math.min(...numbers);
-	return 0;
+	resultArray.splice(resultArray.indexOf(min), 1);
+	return resultArray;
 }
 removeSmallest([1, 2, 3, 4, 5]);
 removeSmallest([2, 2, 2, 1, 2, 1]);
+
+// Best practice
+function removeSmallest2(numbers) {
+	let indexOfMin = numbers.indexOf(Math.min(...numbers));
+	return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
+removeSmallest2([1, 2, 3, 4, 5]);
+removeSmallest2([2, 2, 2, 1, 2, 1]);
