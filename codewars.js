a11d60aa866af1ed450a117731f7912a9935bf07
olepak <oleksandr.lepak@gmail.com>
2024-03-06 16:57:20 +0200

    7 kyu Find the divisors
    Limiting the number of iterations by dividing the whole
    range by 2.

--- codewars.js
+++ codewars.js
@@ -10,3 +10,12 @@ function divisors(integer) {
 	} else return `${integer} is prime`;
 }
 divisors(12);
+
+// Best practice
+function divisors2(integer) {
+	let res = [];
+	for (let i = 2; i <= Math.floor(integer / 2); ++i)
+		if (integer % i === 0) res.push(i);
+	return res.length ? res : integer + " is prime";
+}
+divisors2(12);
