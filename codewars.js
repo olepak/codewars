02529b01d6cae015cb8d8e0d941d5d58bb30fb98
olepak <oleksandr.lepak@gmail.com>
2024-03-08 16:58:53 +0200

    7 kyu Odd or even finish
    reduce() method gives a value to an empty array, if there is a specified
    initial value of total

--- codewars.js
+++ codewars.js
@@ -1,21 +1,4 @@
-function divisors(integer) {
-	let result = [];
-	for (let i = 2; i < integer; i++) {
-		if (integer % i === 0) {
-			result.push(i);
-		}
-	}
-	if (result.length) {
-		return result;
-	} else return `${integer} is prime`;
+function oddOrrEven(array) {
+	return array.reduce((total, num) => total + num, 0) % 2 ? "odd" : "even";
 }
-divisors(12);
-
-// Best practice
-function divisors2(integer) {
-	let res = [];
-	for (let i = 2; i <= Math.floor(integer / 2); ++i)
-		if (integer % i === 0) res.push(i);
-	return res.length ? res : integer + " is prime";
-}
-divisors2(12);
+oddOrrEven([]);
