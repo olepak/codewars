e00610f1b3d7843e7ef413d3e93663f7fbe8af40
olepak <oleksandr.lepak@gmail.com>
2024-03-11 17:44:55 +0200

    7 kyu Sum of odd numbers Start

--- codewars.js
+++ codewars.js
@@ -1,4 +1,14 @@
-function oddOrrEven(array) {
-	return array.reduce((total, num) => total + num, 0) % 2 ? "odd" : "even";
+function oddNumbers(n) {
+	let array = [];
+	for (let i = 1; i < n * (n + 1); i++)
+		if (i % 2 !== 0) {
+			array.push(i);
+		}
+	const triangleArray = [];
+	for (let j = 1; j <= n; j++) {
+		let newArray = array.splice(0, j);
+		triangleArray.push(newArray);
+	}
+	return triangleArray[n - 1].reduce((total, value) => total + value);
 }
-oddOrrEven([]);
+oddNumbers(42);
